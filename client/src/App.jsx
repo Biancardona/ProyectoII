f883f1def3e1e5b38dc7ed60c589149699c2efc8
biancardona <biancardona.rivera@gmail.com>
2024-11-07 14:05:43 -0600

    Historia0. Organizacion de la Estructura del Proyecto

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          PROYECTO II
+        </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
