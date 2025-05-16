9029079667eef01df4ffc8315d74f4132b4bacc2
biancardona <biancardona.rivera@gmail.com>
2025-05-16 16:09:59 -0600

    adding Banner

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Products from '../components/Products';
 import productsData from '../mocks/products.json';
 import { Link } from 'react-router-dom';
+import Banner from '../components/Banner';
 
 const Home = () => {
   const products = productsData;
@@ -9,35 +10,38 @@ const Home = () => {
   const minimalTees = products.filter((p) => p.category === 'MT');
 
   return (
-    <div className='p-4 space-y-12'>
-      <section>
-        <div className='flex justify-between items-center mb-4'>
-          <h2 className='text-3xl font-bold text-center text-indigo-600'>
-            Street Wear
-          </h2>
-          <Link
-            to='/tienda/SW'
-            className='text-indigo-400 hover:underline hover:text-indigo-600'
-          >
-            Ver todo &rarr;
-          </Link>
-        </div>
-        <Products products={streetWear.slice(0, 4)} />
-      </section>
-      <section>
-        <div className='flex justify-between items-center mb-4'>
-          <h2 className='text-3xl text-center font-bold text-pink-600'>
-            Minimal Tees
-          </h2>
-          <Link
-            to='/tienda/MT'
-            className='text-pink-400 hover:underline hover:text-pink-600'
-          >
-            Ver todo &rarr;
-          </Link>
-        </div>
-        <Products products={minimalTees.slice(0, 4)} />
-      </section>
+    <div>
+      <Banner />
+      <div className='p-4 space-y-12'>
+        <section>
+          <div className='flex justify-between items-center mb-4'>
+            <h2 className='text-3xl font-bold text-center text-indigo-600'>
+              Street Wear
+            </h2>
+            <Link
+              to='/tienda/SW'
+              className='text-indigo-400 hover:underline hover:text-indigo-600'
+            >
+              Ver todo &rarr;
+            </Link>
+          </div>
+          <Products products={streetWear.slice(0, 4)} />
+        </section>
+        <section>
+          <div className='flex justify-between items-center mb-4'>
+            <h2 className='text-3xl text-center font-bold text-pink-600'>
+              Minimal Tees
+            </h2>
+            <Link
+              to='/tienda/MT'
+              className='text-pink-400 hover:underline hover:text-pink-600'
+            >
+              Ver todo &rarr;
+            </Link>
+          </div>
+          <Products products={minimalTees.slice(0, 4)} />
+        </section>
+      </div>
     </div>
   );
 };
