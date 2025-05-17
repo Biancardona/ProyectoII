7c99eec22f45ffa84d21411c5df37fab87a80ffe
biancardona <biancardona.rivera@gmail.com>
2025-05-17 17:42:05 -0600

    Renaming home banner

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
   return (
     <div className='w-full overflow-hidden'>
       <img
-        src='/Imagenes/banner.webp'
+        src='/Imagenes/bannerhome.webp'
         alt='Imagen principal'
         className='w-full h-48 md:h-64 lg:h-[500px] object-cover shadow-md'
       />
