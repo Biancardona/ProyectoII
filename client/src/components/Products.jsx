9810673e95b506e1285c291682f37e1966d2eee8
biancardona <biancardona.rivera@gmail.com>
2025-05-17 18:11:35 -0600

    Fixing mobile view in products

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -2,7 +2,7 @@ import Product from './Product';
 
 const Products = ({ products }) => {
   return (
-    <div className='grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
+    <div className='grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4'>
       {products.length ? (
         <>
           {products.slice(0, 20).map((product) => (
